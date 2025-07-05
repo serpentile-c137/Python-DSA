abd7f440c3122f7a3b53bc430d5ac3bc56e36fcf
serpentile-c137 <shardulgg972001@gmail.com>
2025-07-05 22:41:25 +0530

    bubble sort

--- SortingAlgorithms/BubbleSort.py
+++ SortingAlgorithms/BubbleSort.py
@@ -11,6 +11,20 @@ def BubbleSort(arr):
     return arr
 
 arr = [64, 34, 25, 12, 22, 11, 90]
+print("Bubble Sort Algorithm")
 print("Unsorted array is:", *arr)
 sorted_arr = BubbleSort(arr)
 print("Sorted array is:", *sorted_arr)
+
+# Output:
+
+# Bubble Sort Algorithm
+# Unsorted array is: 64 34 25 12 22 11 90
+# Pass 1:  34 25 12 22 11 64 90
+# Pass 2:  25 12 22 11 34 64 90
+# Pass 3:  12 22 11 25 34 64 90
+# Pass 4:  12 11 22 25 34 64 90
+# Pass 5:  11 12 22 25 34 64 90
+# Pass 6:  11 12 22 25 34 64 90
+# Pass 7:  11 12 22 25 34 64 90
+# Sorted array is: 11 12 22 25 34 64 90
