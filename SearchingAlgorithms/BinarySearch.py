7e9ce21bd5a1f1bd0aa1e5679430f6766ab4e459
Shardul Gore <shardulgore@Sharduls-MacBook-Air.local>
2025-07-05 22:19:01 +0530

    binary search

---
+++ SearchingAlgorithms/BinarySearch.py
@@ -0,0 +1,25 @@
+def BinarySearch(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return "Not Found"
+
+
+arr = [1, 2, 3, 4, 5]
+target = 3
+result = BinarySearch(arr, target)
+print("array: ", *arr)
+print(f"Element {target} found at index: {result}")
+
+target = 6
+result = BinarySearch(arr, target)
+print("array: ", *arr)
+print(f"Element {target} found at index: {result}")
