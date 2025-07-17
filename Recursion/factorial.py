459b2c852662407a30a3fde856d9659fd8b6d06c
serpentile-c137 <shardulgg972001@gmail.com>
2025-07-17 12:07:18 +0530

    factorial

---
+++ Recursion/factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    assert n >= 0, "n must be a non-negative integer"
+    if n in [0,1]:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(f"Factorial of 5 is: {factorial(5)}")
